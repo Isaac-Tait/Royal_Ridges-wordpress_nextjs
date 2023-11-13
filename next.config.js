51585d11e87b86d26ded055e4fbe37a2caa29e29
Isaac Tait <isaac@mountaintopcoding.com>
2023-11-13 16:08:39 -0500

    feat: first commit 🥦

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'royal-ridges-retreat.s3.us-west-1.amazonaws.com',
+      },
+    ],
+  },
+};
---
+++ src/components/Wordpress.js
@@ -0,0 +1,30 @@
+import { getAllPosts } from '../../src/pages/api/api';
+
+const Wordpress = ({ allPosts }) => {
+  // Check if allPosts and edges are defined before accessing their properties
+  const edges = allPosts ? allPosts.edges : [];
+
+  return (
+    <section>
+      {edges.map(({ node }) => (
+        <div key={node.id}>
+          <p className='text-3xl font-cursive text-black'>
+            {node.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default Wordpress;
+
+export async function getStaticProps() {
+  const allPosts = await getAllPosts();
+
+  return {
+    props: {
+      allPosts,
+    },
+  };
+}
