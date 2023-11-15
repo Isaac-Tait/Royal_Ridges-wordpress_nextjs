7bcbb033b01140e944bed163c7ad24cd0396cdb5
Isaac Tait <isaac@mountaintopcoding.com>
2023-11-15 10:04:29 -0500

    fix: disable search funct for now 📡

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 
 import algoliasearch from 'algoliasearch';
 import {
@@ -7,7 +7,7 @@ import {
   Hits,
 } from 'react-instantsearch-dom';
 
-//import SearchPreview from '../../(components)/SearchPreview';
+import SearchPreview from '../../(components)/SearchPreview';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -22,19 +22,16 @@ class Search extends React.Component {
       <div className='bg-gradient-to-l from-green-200 to-yellow-200'>
         <Header />
         <div class='max-w-6xl h-screen flex flex-col mx-auto overflow-y-scroll'>
-          {/*👇🏼 Search component*/}
           <InstantSearch
             searchClient={searchClient}
             indexName='Pages'
           >
-            {/*👇🏼 Search Box */}
             <div class='flex justify-center content-center mt-4'>
               <p className='text-green-200 font-semibold mr-2'>
                 Enter your search query:
               </p>
               <SearchBox />
             </div>
-            {/*👇🏼 Hits */}
             <div className='my-4'>
               <Hits hitComponent={SearchPreview} />
             </div>
@@ -47,3 +44,4 @@ class Search extends React.Component {
 }
 
 export default Search;
+ */
