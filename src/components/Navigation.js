9a411d61736ed6a9aac2cca58707c9923c0638ab
Isaac Tait <isaac@mountaintopcoding.com>
2023-11-22 13:37:07 -0500

    fix: last minute touch ups 🙈

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,15 @@
-"use client"
+'use client';
 import { Disclosure } from '@headlessui/react';
 import { XIcon } from '@heroicons/react/outline';
 
 const navigation = [
   { name: 'Programs', href: '/programs', current: false },
   { name: 'Summer Camp', href: '/summer-camp', current: false },
-  { name: 'Updates', href: '/updates', current: false },
+  {
+    name: 'Updates',
+    href: 'https://updatesroyalridges.org/',
+    current: false,
+  },
   { name: 'Support', href: '/support', current: false },
   { name: 'About', href: '/about', current: false },
   { name: 'Contact', href: '/contact', current: false },
@@ -23,27 +27,30 @@ function classNames(...classes) {
 const Navigation = () => {
   return (
     <div>
-      <Disclosure as="nav">
+      <Disclosure as='nav'>
         {({ open }) => (
           <>
-            <div className="max-w-7xl mx-auto px-2 lg:px-8">
-              <div className="relative flex items-center justify-between h-16">
-                <div className="w-full flex justify-center items-center sm:hidden">
+            <div className='max-w-7xl mx-auto px-2 lg:px-8'>
+              <div className='relative flex items-center justify-between h-16'>
+                <div className='w-full flex justify-center items-center sm:hidden'>
                   {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-white hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
+                  <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-white hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                    <span className='sr-only'>Open main menu</span>
                     {open ? (
-                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                      <XIcon
+                        className='block h-6 w-6'
+                        aria-hidden='true'
+                      />
                     ) : (
-                      <p className="underline underline-offset-2 font-cursive text-lg font-medium tracking-wide">
+                      <p className='underline underline-offset-2 font-cursive text-lg font-medium tracking-wide'>
                         Menu
                       </p>
                     )}
                   </Disclosure.Button>
                 </div>
-                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                  <div className="hidden sm:block sm:ml-6 w-full">
-                    <div className="flex space-x-2 justify-center">
+                <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
+                  <div className='hidden sm:block sm:ml-6 w-full'>
+                    <div className='flex space-x-2 justify-center'>
                       {navigation.map((item) => (
                         <a
                           key={item.name}
@@ -52,9 +59,11 @@ const Navigation = () => {
                             item.current
                               ? 'bg-gray-900 text-white'
                               : 'text-gray-800 hover:bg-green-200 hover:text-white',
-                            'px-3 py-2 rounded-md text-base font-semibold',
+                            'px-3 py-2 rounded-md text-base font-semibold'
                           )}
-                          aria-current={item.current ? 'page' : undefined}
+                          aria-current={
+                            item.current ? 'page' : undefined
+                          }
                         >
                           {item.name}
                         </a>
@@ -65,18 +74,18 @@ const Navigation = () => {
               </div>
             </div>
 
-            <Disclosure.Panel className="sm:hidden">
-              <div className="px-2 pt-2 pb-3 space-y-1">
+            <Disclosure.Panel className='sm:hidden'>
+              <div className='px-2 pt-2 pb-3 space-y-1'>
                 {navigation.map((item) => (
                   <Disclosure.Button
                     key={item.name}
-                    as="a"
+                    as='a'
                     href={item.href}
                     className={classNames(
                       item.current
                         ? 'bg-gray-900 text-white'
                         : 'text-gray-800 hover:bg-green-200 hover:text-white',
-                      'block px-3 py-2 rounded-md text-base font-medium',
+                      'block px-3 py-2 rounded-md text-base font-medium'
                     )}
                     aria-current={item.current ? 'page' : undefined}
                   >
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,7 +1,38 @@
+import Link from 'next/link';
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 const search = () => {
   return (
-    <div>
-      <p>This is the temp search page...</p>
+    <div className='h-screen heropattern-topography-yellow-400'>
+      <Header />
+      <p className='h-1/4 opacity-80 bg-gray-100 max-w-5xl mx-auto pt-10 p-1 rounded-md'>
+        This is the temporary search page while our amazing team of
+        web developers (okay there is only one) work with{' '}
+        <a
+          href='https://www.algolia.com/'
+          className='text-green-200 hover:underline hover:text-indigo-400'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Algolia
+        </a>{' '}
+        to build a really cool search solution for our site. If you
+        need help finding anything in the meantime please do not
+        hesitate to reach out to us directly. You can find all our
+        contact information{' '}
+        <Link
+          href='/contact'
+          className='text-green-200 hover:underline hover:text-indigo-400'
+        >
+          on our contact page, which you can find by clicking this
+          link here
+        </Link>
+      </p>
+      <div className='fixed bottom-0 w-full'>
+        <Footer />
+      </div>
     </div>
   );
 };
