a705be67734beea904bd7c083e7586602b508ebd
Isaac Tait <isaac@mountaintopcoding.com>
2024-01-30 14:51:49 -0500

    chore: getting closer 🚁

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,18 +5,17 @@ import {
   InstantSearchSSRProvider,
   getServerState,
   Hits,
+  SearchBox,
 } from 'react-instantsearch';
+
+import SearchHit from '../components/SearchHit';
 
 function Hit({ hit }) {
   return JSON.stringify(hit);
 }
 
-/* const AlgoliaAppId = 'latency';
-const AlgoliaSearchOnlyApiKey = '6be0576ff61c053d5f9a3225e2a90f76';
-const AlgoliaIndexName = 'instant_search'; */
-
 const appId = '9Y3RHNIFAD';
-const searchOnlyApiKey = '6be0576ff61c053d5f9a3225e2a90f76';
+const searchOnlyApiKey = '602c4a47f7f3b3bfd06064bbf6d86ccc';
 const indexName =
   'netlify_3f1d04a0-b847-4a3a-aba8-722f05e29701_main_all';
 
@@ -28,7 +27,11 @@ export default function SearchPage({ serverState }) {
         searchClient={searchClient}
         indexName={indexName}
       >
-        <Hits hitComponent={Hit} />
+        <SearchBox
+          placeholder='Search parameter'
+          className='bg-red-200 flex justify-center'
+        />
+        <Hits hitComponent={SearchHit} />
       </InstantSearch>
     </InstantSearchSSRProvider>
   );
