88f39f60c0fffd38e716fea40a39068140f299a4
Isaac Tait <isaac@mountaintopcoding.com>
2024-08-01 08:51:45 -0400

    fix: update forms to runtime v5 ✋🏼

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Head from 'next/head';
 
+import Form from '../components/Form';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -73,61 +74,9 @@ const Comments = () => {
             Or you can in touch with us by filling out the form.
           </p>
         </div>
-        <form
-          netlify-honeypot='bot-field'
-          name='contact'
-          method='POST'
-          data-netlify='true'
-          action='/success'
-          className='w-full'
-        >
-          <input type='hidden' name='form-name' value='contact' />
-          <div className='my-4 flex flex-col'>
-            <p class='hidden'>
-              <label>
-                Don&39s;t fill this out if you&39s;re human:{' '}
-                <input name='bot-field' />
-              </label>
-            </p>
-            <label htmlFor='name'>Name</label>
-            <input
-              className='border-2 border-green-200 caret-yellow-400 w-1/4'
-              type='text'
-              name='name'
-            />
-          </div>
-          <div className='mb-4 flex flex-col'>
-            <label htmlFor='email'>Email</label>
-            <input
-              className='border-2 border-green-200 caret-yellow-400 w-1/4'
-              type='email'
-              name='email'
-            />
-          </div>
-          <div className='mb-4 flex flex-col'>
-            <label htmlFor='phone'>Phone</label>
-            <input
-              className='border-2 border-green-200 caret-yellow-400 w-1/4'
-              type='text'
-              name='phone'
-            />
-          </div>
-          <div className='mb-4 flex flex-col'>
-            <label htmlFor='message'>Message</label>
-            <textarea
-              className='overflow-y-auto h-60 border-2 border-green-200 caret-yellow-400'
-              type='text'
-              name='message'
-            />
-          </div>
-
-          <button
-            type='submit'
-            className='bg-indigo-200 text-gray-700 hover:bg-yellow-200 hover:text-green-200 hover:underline rounded-2xl w-1/4 flex mx-auto cursor-pointer'
-          >
-            <p className='mx-auto'>Send</p>
-          </button>
-        </form>
+        <div className='w-full flex justify-center'>
+          <Form />
+        </div>
       </div>
       <Footer />
     </div>
