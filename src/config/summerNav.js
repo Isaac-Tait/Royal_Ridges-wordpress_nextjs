b08998f41c50150789d57df020e23e39d87d1463
Isaac-Tait <isaac@mountaintopcoding.com>
2025-11-09 19:29:18 -0800

    chore: update menu order 👩🏼‍🎨

--- src/config/summerNav.js
+++ src/config/summerNav.js
@@ -1,10 +1,10 @@
 const SUMMER_NAV = [
   { name: "Registration", href: "/registration" },
-  { name: "Sports Camp", href: "/sports-camp" },
+  { name: "Theater Camp", href: "/theater-camp" },
+  { name: "Arts and Crafts Camp", href: "/art-craft-camp" },
   { name: "Base Camp", href: "/base-camp" },
   { name: "Recreational Sports Camp", href: "/rec-sport-camp" },
-  { name: "Arts and Crafts Camp", href: "/art-craft-camp" },
-  { name: "Theater Camp", href: "/theater-camp" },
+  { name: "Sports Camp", href: "/sports-camp" },
   { name: "Horse Camp", href: "/horse-camp" },
   { name: "Adventure Camp", href: "/adventure-camp" },
   { name: "Paintball Camp", href: "/paintball-camp" },
