142dc791c835a144ac6b9be8b12adddc68b0eedd
Isaac-Tait <isaac@mountaintopcoding.com>
2025-11-14 20:24:26 -0800

    fix: alphabetize the summer nav 🏣

--- src/config/summerNav.js
+++ src/config/summerNav.js
@@ -1,16 +1,16 @@
 const SUMMER_NAV = [
-  { name: "Registration", href: "/registration" },
-  { name: "Theater Camp", href: "/theater-camp" },
+  { name: "Adventure Camp", href: "/adventure-camp" },
   { name: "Arts and Crafts Camp", href: "/art-craft-camp" },
   { name: "Base Camp", href: "/base-camp" },
-  { name: "Recreational Sports Camp", href: "/rec-sport-camp" },
-  { name: "Sports Camp", href: "/sports-camp" },
-  { name: "Horse Camp", href: "/horse-camp" },
-  { name: "Adventure Camp", href: "/adventure-camp" },
-  { name: "Paintball Camp", href: "/paintball-camp" },
-  { name: "Specialty Horse Camp", href: "/specialty-horse-camp" },
   { name: "Bus Transportation", href: "/bus-transportation" },
   { name: "FAQs", href: "/faq" },
+  { name: "Horse Camp", href: "/horse-camp" },
+  { name: "Paintball Camp", href: "/paintball-camp" },
+  { name: "Recreational Sports Camp", href: "/rec-sport-camp" },
+  { name: "Registration", href: "/registration" },
+  { name: "Specialty Horse Camp", href: "/specialty-horse-camp" },
+  { name: "Sports Camp", href: "/sports-camp" },
+  { name: "Theater Camp", href: "/theater-camp" },
 ];
 
 export default SUMMER_NAV;
