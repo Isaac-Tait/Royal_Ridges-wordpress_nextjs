b1427ec3b58518c26292386cd37778c2d8bc39be
Isaac-Tait <isaac@mountaintopcoding.com>
2026-02-24 18:08:13 -0800

    fix: missing comma 🐗

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,17 +1,21 @@
 const NAV = [
-  { name: 'Programs', href: '/programs' },
-  { name: 'Summer Camp', href: '/summer-camp' },
+  { name: "Programs", href: "/programs" },
+  { name: "Summer Camp", href: "/summer-camp" },
   {
-    name: 'Updates',
-    href: 'https://updatesroyalridges.org/',
+    name: "Updates",
+    href: "https://updatesroyalridges.org/",
     external: true,
   },
-  { name: 'Support', href: '/support' },
-  { name: 'About', href: '/about' },
-  { name: 'Contact', href: '/contact' },
+  { name: "Support", href: "/support" },
   {
-    name: 'Account',
-    href: 'https://www.ultracamp.com/clientlogin.aspx?idCamp=1145&campCode=151',
+    name: "Donations",
+    href: "https://funraise.org/give/Royal-Ridges-Camps/7e26b9b5-7472-449b-8698-6d56191ef3c4",
+  },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+  {
+    name: "Account",
+    href: "https://www.ultracamp.com/clientlogin.aspx?idCamp=1145&campCode=151",
     external: true,
   },
 ];
