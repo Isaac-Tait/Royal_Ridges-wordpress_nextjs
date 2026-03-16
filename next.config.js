dbc21678fc91482fae7f3af4ba8e48578a99892c
Isaac-Tait <isaac@mountaintopcoding.com>
2026-03-16 11:20:44 -0700

    fix: Add security headers to prevent clickjacking 🐉

--- next.config.js
+++ next.config.js
@@ -2,17 +2,43 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
       {
-        protocol: 'https',
-        hostname: 'royal-ridges-retreat.s3.us-west-1.amazonaws.com',
+        protocol: "https",
+        hostname: "royal-ridges-retreat.s3.us-west-1.amazonaws.com",
       },
       {
-        protocol: 'https',
-        hostname: 'updatesroyalridges.org',
+        protocol: "https",
+        hostname: "updatesroyalridges.org",
       },
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self';",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
   },
 };
